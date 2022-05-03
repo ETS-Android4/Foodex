a67556925224df2113a61f1cfc5528e6434da7e1
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2022-05-03 22:55:44 +0200

    Firebase connected, Register function works

---
+++ Foodex/app/src/main/java/com/example/foodex/User.java
@@ -0,0 +1,19 @@
+package com.example.foodex;
+
+public class User {
+
+    public String fullName, email;
+
+    public User(){
+
+    }
+
+    public User(String fullName, String email)
+    {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+
+
+}
