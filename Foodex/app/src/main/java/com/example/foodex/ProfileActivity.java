f38b9dacd47830a1c1ecbb9d797259727243a6b7
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2022-05-04 10:47:37 +0200

    Bottom Nav bar created

--- Foodex/app/src/main/java/com/example/foodex/ProfileActivity.java
+++ Foodex/app/src/main/java/com/example/foodex/ProfileActivity.java
@@ -1,15 +1,25 @@
 package com.example.foodex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.foodex.fragments.FavoritesFragment;
+import com.example.foodex.fragments.HomeFragment;
+import com.example.foodex.fragments.PantryFragment;
+import com.example.foodex.fragments.ProfileFragment;
+import com.example.foodex.fragments.ShoppingListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity{
 
     private Button logout;
 
@@ -26,5 +36,63 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(ProfileActivity.this, MainActivity.class));
             }
         });
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId())
+            {
+                case R.id.nav_pantry:
+                    selectedFragment = new PantryFragment();
+                    break;
+
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.nav_favorites:
+                    selectedFragment = new FavoritesFragment();
+                    break;
+
+                case R.id.nav_shopping_list:
+                    selectedFragment = new ShoppingListFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
+
+//    @Override
+//    public void onClick(View view) {
+//        Fragment selectedFragment = null;
+//        switch (view.getId()) {
+//            case R.id.nav_pantry:
+//                selectedFragment = new PantryFragment();
+//                break;
+//
+//            case R.id.nav_home:
+//                selectedFragment = new HomeFragment();
+//                break;
+//
+//            case R.id.nav_favorites:
+//                selectedFragment = new FavoritesFragment();
+//                break;
+//
+//            case R.id.nav_shopping_list:
+//                selectedFragment = new ShoppingListFragment();
+//                break;
+//        }
+//        if (selectedFragment != null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+//        }
+//    }
+
 }
